aeabfe4da0f40e164116af7ea6bbbe39cd3473ea
ser-tec <113360845+ser-tec@users.noreply.github.com>
2023-02-27 09:17:59 +0100

    Update update_custom_addons.sh

--- update_custom_addons.sh
+++ update_custom_addons.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-dir=$(dirname $0)
-cd $dir
-
 # Import the configuration file
 . autoupdate.conf
 
+var_folders=("${CUSTOM_FOLDERS[@]}")
+variable_folders=$(ls -d $var_folders/* 2>/dev/null)
+
 # loop through all folders in $CUSTOM_FOLDER
-for folder in "${CUSTOM_FOLDER[@]}"; do
+for folder in $variable_folders; do
     echo "Updating folder: $folder"
     cd "$folder"
     git checkout || true
     git pull
-
-    # loop through all subfolders in $folder
-    for subfolder in */; do
-        echo "Updating subfolder: $subfolder"
-        cd "$subfolder"
-        git checkout || true
-        git pull
-        cd ..
-    done
 done
