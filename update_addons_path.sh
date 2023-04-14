8d53de5afda32b4b979141a662ce52586be6aa11
ser-tec <113360845+ser-tec@users.noreply.github.com>
2023-04-14 16:56:53 +0200

    Update update_addons_path.sh

--- update_addons_path.sh
+++ update_addons_path.sh
@@ -7,14 +7,18 @@ echo $PWD
 # Import configuration file
 . update.conf
 
+echo $OCA_FOLDER
+echo "###"
+
 # Folders list
 DEFAULT_FOLDERS="$ODOO_PATH/addons,$ODOO_PATH/odoo/addons"
-OCA_FOLDER=$(ls -d $OCA_FOLDER/* 2>/dev/null)
+OCA_FOLDER1=$(ls -d $OCA_FOLDER/* 2>/dev/null)
 GIT_ADDONS_FOLDERS=${GIT_ADDONS_FOLDERS[@]}
 OTHER_FOLDERS=${OTHER_FOLDERS[@]}
-
+echo $OCA_FOLDER1
+echo "###"
 # Format the folders list
-OCA_FOLDER2=$(echo "$OCA_FOLDER" | tr ' ' ',')
+OCA_FOLDER2=$(echo "$OCA_FOLDER1" | tr ' ' ',')
 GIT_ADDONS_FOLDERS=$(echo "$GIT_ADDONS_FOLDERS" | tr ' ' ',')
 OTHER_FOLDERS=$(echo "$OTHER_FOLDERS" | tr ' ' ',')
 ALL_FOLDERS="$OTHER_FOLDERS,$GIT_ADDONS_FOLDERS,$DEFAULT_FOLDERS,$OCA_FOLDER"
