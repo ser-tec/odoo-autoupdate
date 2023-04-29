6d07fb8dff57ad3b304f2e461755b21488a27d1f
ser-tec <113360845+ser-tec@users.noreply.github.com>
2023-04-29 18:14:53 +0200

    Update update_git_folders.sh

--- update_git_folders.sh
+++ update_git_folders.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-dir=$(dirname $0)
-cd $dir
+dir=$(dirname "$0")
+cd "$dir"
 
 # Import the configuration file
 . update.conf
 
 # loop through all folders in $GIT_ADDONS_FOLDERS
-for i in "${GIT_ADDONS_FOLDERS[@]}"
-  do
-    echo "Updating folder: " $GIT_ADDONS_FOLDERS[$i]
-    cd $GIT_ADDONS_FOLDERS[$i]
-    git checkout || true
-    git pull
+for i in "${GIT_ADDONS_FOLDERS[@]}"; do
+  echo "Updating folder: $i"
+  cd "$i"
+  git checkout || true
+  git pull
+  cd "$dir"
 done
-
