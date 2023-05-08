2f75e657da488446fdbb461739d5fb1241dc0194
ser-tec <113360845+ser-tec@users.noreply.github.com>
2023-05-08 09:28:10 +0200

    Update update_addons_path.sh
    rimuovo virgola all'inizio variabile nel caso other folder vuoto

--- update_addons_path.sh
+++ update_addons_path.sh
@@ -20,6 +20,7 @@ ALL_FOLDERS="$OTHER_FOLDERS,$GIT_ADDONS_FOLDERS,$DEFAULT_FOLDERS,$OCA_FOLDER"
 
 # remove possible comma error
 ALL_FOLDERS=$(echo "$ALL_FOLDERS" | tr -s ',')
+ALL_FOLDERS=$(echo "$ALL_FOLDERS" | sed 's/^,//')
 
 #echo $ALL_FOLDERS
 echo "write on update.conf"
